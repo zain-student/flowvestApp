a7a34be221a09c46bfa221134fc70aa997c6316e
Zain ul Abideen <zainmalik4989@gmail.com>
2026-03-13 15:21:12 +0500

    Updated investments detail and form for adding amount detail to add partner to investment

--- src/modules/Investor/investments/screens/AddInvestmentPartner.tsx
+++ src/modules/Investor/investments/screens/AddInvestmentPartner.tsx
@@ -1,349 +1,3 @@
-// import { showToast } from "@/modules/auth/utils/showToast";
-// import { InvestmentStackParamList } from "@/navigation/InvestorStacks/InvestmentStack";
-// import Colors from "@/shared/colors/Colors";
-// import { Button } from "@/shared/components/ui";
-// import { useAppDispatch, useAppSelector } from "@/shared/store";
-// import {
-//   fetchPartners,
-//   invitePartnerToInvestment,
-// } from "@/shared/store/slices/investor/dashboard/addPartnerSlice";
-// import { useCurrencyFormatter } from "@/shared/utils/useCurrencyFormatter";
-// import { Ionicons } from "@expo/vector-icons";
-// import { RouteProp, useRoute } from "@react-navigation/native";
-// import React, { useEffect, useMemo, useState } from "react";
-// import {
-//   ActivityIndicator,
-//   FlatList,
-//   StyleSheet,
-//   Text,
-//   TextInput,
-//   TouchableOpacity,
-//   View,
-// } from "react-native";
-
-// type AddPartnerRouteProp = RouteProp<InvestmentStackParamList, "AddPartner">;
-
-// interface Partner {
-//   id: number;
-//   name: string;
-//   email: string;
-// }
-
-// export default function AddInvestmentPartner() {
-//   const route = useRoute<AddPartnerRouteProp>();
-//   const { id } = route.params;
-//   const dispatch = useAppDispatch();
-
-//   const { partners, isLoading } = useAppSelector((state) => state.partner);
-//   const { investments } = useAppSelector((state) => state.investments);
-//   const { formatCurrency } = useCurrencyFormatter();
-//   const [selectedPartner, setSelectedPartner] = useState<Partner | null>(null);
-//   const [investedAmount, setInvestedAmount] = useState("");
-//   const [investmentNotes, setInvestmentNotes] = useState("");
-//   const [invitationMessage, setInvitationMessage] = useState("");
-//   const [minExperience, setMinExperience] = useState("");
-
-//   // Get investment details
-//   const investment = useMemo(
-//     () => investments.find((inv) => inv.id === id),
-//     [investments, id],
-//   );
-
-//   useEffect(() => {
-//     dispatch(fetchPartners());
-//   }, [dispatch]);
-
-//   const handleAddPartner = async () => {
-//     if (
-//       !selectedPartner
-//       // || !investedAmount
-//     ) {
-//       showToast("Please select a partner first!.");
-//       return;
-//     }
-
-//     const result = await dispatch(
-//       invitePartnerToInvestment({
-//         investmentId: id,
-//         partnerId: selectedPartner.id,
-//         investedAmount: parseFloat(investedAmount),
-//         investmentNotes,
-//         invitationMessage,
-//         minExperience,
-//       }),
-//     );
-
-//     if (invitePartnerToInvestment.fulfilled.match(result)) {
-//       showToast("Partner invited successfully!");
-//       resetForm();
-//     } else {
-//       showToast(
-//         (result.payload as string) || "Failed to invite partner",
-//         "error",
-//       );
-//     }
-//   };
-
-//   const resetForm = () => {
-//     setSelectedPartner(null);
-//     setInvestedAmount("");
-//     setInvestmentNotes("");
-//     setInvitationMessage("");
-//     setMinExperience("");
-//   };
-
-//   if (isLoading) {
-//     return (
-//       <View style={styles.loaderContainer}>
-//         <ActivityIndicator size="large" color={Colors.primary} />
-//       </View>
-//     );
-//   }
-
-//   if (!investment) {
-//     return (
-//       <View style={styles.loaderContainer}>
-//         <Text style={{ color: Colors.gray, fontSize: 16 }}>
-//           Investment not found.
-//         </Text>
-//       </View>
-//     );
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <FlatList
-//         data={partners}
-//         keyExtractor={(item) => item.id.toString()}
-//         showsVerticalScrollIndicator={false}
-//         contentContainerStyle={styles.list}
-//         ListHeaderComponent={
-//           <View style={styles.card}>
-//             <Text style={styles.cardTitle}>Investment Details</Text>
-//             <View
-//               style={{
-//                 flexDirection: "row",
-//                 justifyContent: "space-between",
-//                 padding: 4,
-//               }}
-//             >
-//               <View>
-//                 <Text style={styles.label}>Name: </Text>
-//                 <Text style={styles.value}>{investment.name}</Text>
-//               </View>
-//               <View>
-//                 <Text style={styles.label}>Status: </Text>
-//                 <Text style={styles.value}>{investment.status}</Text>
-//               </View>
-//             </View>
-//             <View
-//               style={{
-//                 flexDirection: "row",
-//                 justifyContent: "space-between",
-//                 padding: 4,
-//               }}
-//             >
-//               <View>
-//                 <Text style={styles.label}>Expected Return: </Text>
-//                 <Text style={styles.value}>
-//                   {" "}
-//                   {parseFloat(investment.expected_return_rate).toFixed(1)}%
-//                 </Text>
-//               </View>
-//               <View>
-//                 <Text style={styles.label}>Total: </Text>
-//                 <Text style={styles.value}>
-//                   {formatCurrency(
-//                     Number(
-//                       investment.type === "solo"
-//                         ? investment.initial_amount
-//                         : investment.current_total_invested,
-//                     ),
-//                   )}
-//                 </Text>
-//               </View>
-//             </View>
-//           </View>
-//         }
-//         renderItem={({ item }) => (
-//           <TouchableOpacity
-//             style={[
-//               styles.partnerItem,
-//               selectedPartner?.id === item.id && styles.selectedPartner,
-//             ]}
-//             onPress={() => setSelectedPartner(item)}
-//           >
-//             <Text style={styles.partnerName}>{item.name}</Text>
-//             <View style={{ flexDirection: "row", alignItems: "center" }}>
-//               <Ionicons name="mail-outline" size={13} color={Colors.gray} />
-//               <Text style={styles.partnerEmail}>{item.email}</Text>
-//             </View>
-//           </TouchableOpacity>
-//         )}
-//         ListEmptyComponent={
-//           <View style={styles.emptyState}>
-//             <Text style={styles.emptyText}>No partner available.</Text>
-//           </View>
-//         }
-//       />
-
-//       {selectedPartner && (
-//         <View style={styles.formCard}>
-//           <Text style={styles.cardTitle}>Add Partner Details</Text>
-
-//           <TextInput
-//             style={styles.input}
-//             placeholder="Investment Amount(Optional)"
-//             placeholderTextColor={Colors.gray}
-//             keyboardType="numeric"
-//             value={investedAmount}
-//             onChangeText={setInvestedAmount}
-//           />
-//           <TextInput
-//             style={styles.input}
-//             placeholder="Investment Notes(Optional)"
-//             placeholderTextColor={Colors.gray}
-//             value={investmentNotes}
-//             onChangeText={setInvestmentNotes}
-//           />
-//           <TextInput
-//             style={styles.input}
-//             placeholder="Invitation Message(Optional)"
-//             placeholderTextColor={Colors.gray}
-//             value={invitationMessage}
-//             onChangeText={setInvitationMessage}
-//           />
-//           <TextInput
-//             style={styles.input}
-//             placeholder="Min Experience(Optional)"
-//             placeholderTextColor={Colors.gray}
-//             value={minExperience}
-//             onChangeText={setMinExperience}
-//           />
-//           <Button
-//             title="Add Partner"
-//             // icon={<Ionicons name="add-circle-outline" size={20} color={Colors.white} />}
-//             disabled={isLoading}
-//             onPress={handleAddPartner}
-//             style={styles.joinButton}
-//             textStyle={styles.joinText}
-//             variant="primary"
-//           />
-//         </View>
-//       )}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     paddingHorizontal: 12,
-//     // paddingBottom: 80,
-//     backgroundColor: "#F9FAFB",
-//   },
-//   loaderContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
-//   list: {
-//     paddingBottom: 75,
-//   },
-//   card: {
-//     // marginHorizontal: 12,
-//     backgroundColor: Colors.white,
-//     borderRadius: 14,
-//     padding: 12,
-//     marginVertical: 2,
-//     borderWidth: 1,
-//     borderColor: "#E6EDFF",
-//   },
-//   cardTitle: {
-//     fontSize: 13,
-//     fontWeight: "500",
-//     color: Colors.gray,
-//     marginBottom: 8,
-//   },
-//   label: {
-//     fontSize: 12,
-//     fontWeight: "500",
-//     lineHeight: 18,
-//     color: Colors.gray,
-//     marginBottom: 4,
-//   },
-//   value: {
-//     color: Colors.secondary,
-//     fontWeight: "600",
-//     fontSize: 14,
-//     alignSelf: "flex-start",
-//   },
-//   detail: {
-//     color: Colors.secondary,
-//     fontSize: 12,
-//     fontFamily: "Inter_400Regular",
-//     flexDirection: "row",
-//     alignItems: "center",
-//   },
-//   partnerItem: {
-//     backgroundColor: Colors.white,
-//     borderRadius: 14,
-//     padding: 12,
-//     marginVertical: 2,
-//     borderWidth: 1,
-//     borderColor: "#E6EDFF",
-//   },
-//   selectedPartner: { borderColor: "#2563EB", backgroundColor: "#EFF6FF" },
-//   partnerName: {
-//     color: Colors.secondary,
-//     fontSize: 15,
-//     fontFamily: "Inter_700Bold",
-//     marginBottom: 2,
-//   },
-//   partnerEmail: {
-//     color: Colors.gray,
-//     fontSize: 12,
-//     fontFamily: "Inter_400Regular",
-//     flexDirection: "row",
-//     alignItems: "center",
-//     marginLeft: 4,
-//   },
-//   emptyState: { justifyContent: "center", alignItems: "center", padding: 20 },
-//   emptyText: { fontSize: 16, color: "#6B7280" },
-//   formCard: {
-//     backgroundColor: "#fff",
-//     borderRadius: 12,
-//     padding: 16,
-//     marginTop: 16,
-//     marginBottom: 10,
-//     elevation: 2,
-//     shadowColor: "#000",
-//     shadowOpacity: 0.05,
-//     shadowRadius: 3,
-//   },
-//   input: {
-//     borderWidth: 1,
-//     borderColor: "#E5E7EB",
-//     borderRadius: 8,
-//     padding: 12,
-//     marginBottom: 12,
-//     backgroundColor: "#F9FAFB",
-//     fontSize: 14,
-//   },
-//   joinButton: {
-//     paddingVertical: 10,
-//     marginTop: 6,
-//     flexDirection: "row",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: Colors.primary,
-//     // paddingVertical: 10,
-//     borderRadius: 10,
-//   },
-
-//   joinText: {
-//     color: "#fff",
-//     fontSize: 14,
-//     fontWeight: "600",
-//     marginLeft: 6,
-//   },
-// });
 import { Ionicons } from "@expo/vector-icons";
 import { RouteProp, useRoute } from "@react-navigation/native";
 import React, { useEffect, useMemo, useState } from "react";
@@ -507,6 +161,26 @@ export default function AddInvestmentPartner() {
             </Text>
           </View>
         </View>
+        <View style={styles.row}>
+          <View>
+            <Text style={styles.label}>Min:</Text>
+            <Text style={styles.value}>
+              {formatInvestmentCurrency(
+                investment.min_investment_amount,
+                investment.currency.code,
+              )}
+            </Text>
+          </View>
+          <View>
+            <Text style={styles.label}>Max:</Text>
+            <Text style={styles.value}>
+              {formatInvestmentCurrency(
+                investment.max_investment_amount,
+                investment.currency.code,
+              )}
+            </Text>
+          </View>
+        </View>
       </View>
 
       {/* Partners List */}
@@ -572,6 +246,17 @@ export default function AddInvestmentPartner() {
               <Text style={{ marginBottom: 4, color: Colors.secondary }}>
                 Investment Amount *
               </Text>
+              <Text style={{ fontSize: 12, color: Colors.gray }}>
+                {formatInvestmentCurrency(
+                  investment.min_investment_amount,
+                  investment.currency.code,
+                )}{" "}
+                -{" "}
+                {formatInvestmentCurrency(
+                  investment.max_investment_amount,
+                  investment.currency.code,
+                )}
+              </Text>
               <TextInput
                 style={styles.input}
                 placeholder="0"
@@ -580,6 +265,7 @@ export default function AddInvestmentPartner() {
                 value={investedAmount}
                 onChangeText={setInvestedAmount}
               />
+
               <Text style={{ marginBottom: 4, color: Colors.secondary }}>
                 Investment Notes (Optional)
               </Text>
